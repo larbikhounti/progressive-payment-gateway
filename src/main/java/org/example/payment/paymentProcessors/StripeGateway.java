6b675b71e6fa4f49a45c185068a3adcd715b483f
mohamed khounti <larbikhounti@gmail.com>
2024-07-15 13:37:19 +0100

    adding stripe test payment processing

---
+++ src/main/java/org/example/payment/paymentProcessors/StripeGateway.java
@@ -0,0 +1,52 @@
+package org.example.payment.paymentProcessors;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.example.config.ConfigUtil;
+import org.example.payment.interfaces.paymentProcessor;
+import org.example.payment.models.Card;
+
+public class StripeGateway implements paymentProcessor {
+
+    /**
+     * pay method
+     * @param card card
+     * @return TransactionId | error
+     **/
+    @Override
+    public String pay(Card card) {
+        //TODO : ADD PAYMENT LOGIC AND RETURN THE REAL RESULTS
+        com.stripe.Stripe.apiKey = ConfigUtil.getProperty("STRIPE.SECRET");
+        PaymentIntentCreateParams params =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(card.getAmount())
+                        .setCurrency("gbp")
+                        .setPaymentMethod("pm_card_visa")
+                        .setConfirm(true)
+                        .setReturnUrl("https://localhost")
+                        .build();
+
+        try {
+            PaymentIntent paymentIntent = PaymentIntent.create(params);
+            // will return payment status
+            return  paymentIntent.getStatus();
+        } catch (StripeException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    /**
+     * check if card is valid
+     * @param card cart
+     * @return true | false
+     */
+    @Override
+    public Boolean checkCardValidation(Card card) {
+        //TODO : ADD CHECKING LOGIC AND RETURN THE REAL RESULTS
+        //in this case Stripe automatically checks Card validation
+        return true;
+    }
+}
